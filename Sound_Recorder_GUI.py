7559a3eb13af0501ce73654dc1f79d7b1f2c0003
muanus <muhammad.anus@tu-ilmenau.de>
2023-12-07 01:42:13 +0100

    Comments Updated

--- Sound_Recorder_GUI.py
+++ Sound_Recorder_GUI.py
@@ -14,15 +14,18 @@ def Voice_rec():
 						samplerate=fs, channels=2) 
 	sd.wait() 
 	
-	# Save as FLAC file at correct sampling rate 
+	# Save the recording as a FLAC file with the correct sampling rate
 	return sf.write('my_Audio_file.flac', myrecording, fs) 
 
 
 master = Tk() 
 
+# Create a label with the text "Voice Recorder:" and add it to the window
+
 Label(master, text=" Voice Recoder : "
 	).grid(row=0, sticky=W, rowspan=5) 
 
+# Create a button that starts the voice recording when clicked
 
 b = Button(master, text="Start", command=Voice_rec) 
 b.grid(row=0, column=2, columnspan=2, rowspan=2, 
