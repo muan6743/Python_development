63bc7c256b2bf50f7207956f1aa71f2c21dea354
muanus <muhammad.anus@tu-ilmenau.de>
2023-12-29 01:57:18 +0100

    Add Olympic rings to the drawing

--- Olympic_Symbol.py
+++ Olympic_Symbol.py
@@ -24,6 +24,43 @@ t.penup()
 t.goto(50, 0)
 t.pendown()
 
+# Draw the second ring (black)
+t.color("black")
+t.pensize(ring_thickness)
+t.circle(50)
+
+# Move to the next position to draw the third ring
+t.penup()
+t.goto(200, 0)
+t.pendown()
+
+# Draw the third ring (red)
+t.color("red")
+t.pensize(ring_thickness)
+t.circle(50)
+
+# Move to the next position to draw the fourth ring
+t.penup()
+t.goto(-25, -50)
+t.pendown()
+
+# Draw the fourth ring (yellow)
+t.color("yellow")
+t.pensize(ring_thickness)
+t.circle(50)
+
+# Move to the next position to draw the fifth ring
+t.penup()
+t.goto(125, -50)
+t.pendown()
+
+# Draw the fifth ring (green)
+t.color("green")
+t.pensize(ring_thickness)
+t.circle(50)
+
+# Hide the turtle
+t.hideturtle()
 
 # Keep the window open
 turtle.done()
